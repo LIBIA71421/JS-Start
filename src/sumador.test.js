478f912a363b3e52efa052a2aa0b084ddf6ba3bc
libia <libia.roman@ucb.edu.bo>
2023-03-16 15:04:19 +0100

    primer test con jest

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,7 +1,14 @@
 import sumar from "./sumador.js";
+import multiplicar from "./multiplicador";
 
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
+  });
+});
+
+describe("Multiplicar", () => {
+  it("deberia multiplicar dos numeros", () => {
+    expect(multiplicar(3, 2)).toEqual(6);
   });
 });
